5d7eba6b795d48e740420842dfdf7945fb350798
michaelguarino <mguarino46@gmail.com>
2021-06-18 22:05:30 -0400

    fix proxy command handling

--- pkg/proxy/shell.go
+++ pkg/proxy/shell.go
@@ -11,9 +11,11 @@ func execShell(namespace string, proxy *v1alpha1.Proxy) error {
 	shell := proxy.Spec.ShConfig
 	var rest []string
 	if len(shell.Command) > 0 {
-		rest = append([]string{"-e", shell.Command}, shell.Args...)
+		rest = append([]string{shell.Command}, shell.Args...)
+	} else {
+		rest = []string{"/bin/sh"}
 	}
-	args := []string{"exec", "-it", "-n", namespace, proxy.Spec.Target, "--", "/bin/sh"}
+	args := []string{"exec", "-it", "-n", namespace, proxy.Spec.Target, "--"}
 	args = append(args, rest...)
 	cmd := exec.Command("kubectl", args...)
 	cmd.Stdout = os.Stdout
