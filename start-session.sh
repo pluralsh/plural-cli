e145e4db77d969e2e63752b367658d1fd490798a
michaelguarino <mguarino46@gmail.com>
2022-01-21 17:28:04 -0500

    add tmux to cli docker container

---
+++ start-session.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+session="workspace"
+tmux start \;
+tmux has-session -t $session 2>/dev/null
+
+if [ $? != 0 ]; then
+  tmux new-session -c /root/workspace -s $session zsh
+fi
+
+# Attach to created session
+tmux attach-session -t $session
