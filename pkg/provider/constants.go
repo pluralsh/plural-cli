20a3271e1714c10933f34d0ccf75d7537accee5f
DavidSpek <vanderspek.david@gmail.com>
2022-02-03 14:24:52 +0100

    bump aws provider version

--- pkg/provider/constants.go
+++ pkg/provider/constants.go
@@ -114,7 +114,7 @@ const awsBackendTemplate = `terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.62.0"
+      version = "~> 3.63.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
