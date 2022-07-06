1ecd6fa50e11dcb9081f9ae06b87a956b7e18fab
Lukasz Zajaczkowski <zreigz@gmail.com>
2022-07-06 10:40:23 +0200

    fix code review comments 1

--- cmd/plural/config.go
+++ cmd/plural/config.go
@@ -64,7 +64,7 @@ func handleAmend(c *cli.Context) error {
 	return config.Amend(c.Args().Get(0), c.Args().Get(1))
 }
 
-func handleRead(*cli.Context) error {
+func handleRead(c *cli.Context) error {
 	conf := config.Read()
 	d, err := conf.Marshal()
 	if err != nil {
