45c51316c802d5eb7a708dd6117ecc2cd20ebd87
Lukasz Zajaczkowski <zreigz@gmail.com>
2022-07-29 08:55:11 +0200

    add new unit tests for Kube methods

---
+++ cmd/plural/proxy_test.go
@@ -0,0 +1,63 @@
+package main_test
+
+import (
+	"os"
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/pluralsh/plural-operator/api/platform/v1alpha1"
+	"github.com/stretchr/testify/mock"
+
+	plural "github.com/pluralsh/plural/cmd/plural"
+	"github.com/pluralsh/plural/pkg/test/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProxyList(t *testing.T) {
+	tests := []struct {
+		name             string
+		args             []string
+		proxyList        *v1alpha1.ProxyList
+		expectedResponse string
+	}{
+		{
+			name: `test "proxy list"`,
+			args: []string{plural.ApplicationName, "proxy", "list", "test"},
+			proxyList: &v1alpha1.ProxyList{
+				TypeMeta: metav1.TypeMeta{},
+				Items: []v1alpha1.Proxy{
+					{
+						ObjectMeta: metav1.ObjectMeta{Name: "proxy-1"},
+						Spec:       v1alpha1.ProxySpec{Type: v1alpha1.Sh, Target: "test-1"},
+					},
+					{
+						ObjectMeta: metav1.ObjectMeta{Name: "proxy-1"},
+						Spec:       v1alpha1.ProxySpec{Type: v1alpha1.Web, Target: "test-2"},
+					},
+				},
+			},
+			expectedResponse: `+---------+------+--------+
+|  NAME   | TYPE | TARGET |
++---------+------+--------+
+| proxy-1 | sh   | test-1 |
+| proxy-1 | web  | test-2 |
++---------+------+--------+
+`,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			client := mocks.NewClient(t)
+			kube := mocks.NewKube(t)
+			kube.On("ProxyList", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(test.proxyList, nil)
+			app := plural.CreateNewApp(&plural.Plural{Client: client, Kube: kube})
+			app.HelpName = plural.ApplicationName
+			os.Args = test.args
+			res, err := captureStdout(app, os.Args)
+			assert.NoError(t, err)
+
+			assert.Equal(t, test.expectedResponse, res)
+		})
+	}
+}
