7c46b4eb67b34346605096f35f3c1a91a32b0e85
Lukasz Zajaczkowski <zreigz@gmail.com>
2022-08-01 08:25:11 +0200

    fix linter

--- cmd/plural/ops_test.go
+++ cmd/plural/ops_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/pluralsh/plural/pkg/test/mocks"
 	"github.com/pluralsh/plural/pkg/utils/git"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
