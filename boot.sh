c04e74802919a71cd3484c4f4380b4c07e086a68
michaeljguarino <mguarino46@gmail.com>
2022-11-21 02:25:42 -0500

    Migration from tmux to screen (#284)
    This will allow us to actually support copy/paste, and perhaps it will also be a bit more robust as it looks like it has an atomic create/attach command.

--- boot.sh
+++ boot.sh
@@ -4,4 +4,6 @@ if [ -f /home/plural/.env ]; then
   source /home/plural/.env
 fi
 
+cd ~/workspace || echo "could not check out workspace repo, ensure it exists and git permissions are correct"
+
 zsh
