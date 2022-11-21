de804e1efeeccfd206e81e96edac02e5e8f6452a
michaeljguarino <mguarino46@gmail.com>
2022-11-21 02:25:42 -0500

    Migration from tmux to screen (#284)
    This will allow us to actually support copy/paste, and perhaps it will also be a bit more robust as it looks like it has an atomic create/attach command.

--- start-session.sh
+++ start-session.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-session="workspace"
+session="plural-workspace"
 
 # ensure necessary env vars are populated
 if [ -f /home/plural/.env ]; then
   source /home/plural/.env
 fi
 
-tmux start
-tmux has-session -t $session 2>/dev/null
-
-if [ $? != 0 ]; then
-  tmux new-session -c ~/workspace -s $session /home/plural/boot.sh
-fi
-
-# Attach to created session
-tmux attach-session -d -t $session
+screen -xR -S $session /home/plural/boot.sh
