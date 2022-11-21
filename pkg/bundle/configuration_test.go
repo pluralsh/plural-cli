303ab745d249e251e1f55a7234390db81ada77a8
Lukasz Zajaczkowski <zreigz@gmail.com>
2022-11-21 08:17:58 +0100

    add missing condition checks (#282)

--- pkg/bundle/configuration_test.go
+++ pkg/bundle/configuration_test.go
@@ -158,3 +158,353 @@ func TestConfigureEnvVariables(t *testing.T) {
 		})
 	}
 }
+
+func TestEvaluateCondition(t *testing.T) {
+	tests := []struct {
+		name          string
+		ctx           map[string]interface{}
+		item          *api.ConfigurationItem
+		context       *manifest.Context
+		repo          string
+		expectedError string
+		envVars       map[string]string
+		expectedValue string
+	}{
+		{
+			name: "test if EQ for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_eq_int",
+					Operation: "EQ",
+					Value:     "123",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_eq_int": 123,
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not EQ for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_eq_int",
+					Operation: "EQ",
+					Value:     "100",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_eq_int": 123,
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+		{
+			name: "test if EQ for STRING",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.String,
+				Condition: &api.Condition{
+					Field:     "test_eq_str",
+					Operation: "EQ",
+					Value:     "abc",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_eq_str": "abc",
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not EQ for STRING",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.String,
+				Condition: &api.Condition{
+					Field:     "test_eq_str",
+					Operation: "EQ",
+					Value:     "abc",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_eq_str": "cda",
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+		{
+			name: "test if GT for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_gt",
+					Operation: "GT",
+					Value:     "99",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_gt": 100,
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not GT for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_gt",
+					Operation: "GT",
+					Value:     "123",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_gt": 99,
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+		{
+			name: "test if GTE for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_gte",
+					Operation: "GTE",
+					Value:     "99",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_gte": 99,
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not GTE for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_gte",
+					Operation: "GTE",
+					Value:     "123",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_gte": 99,
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+		{
+			name: "test if LT for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_lt",
+					Operation: "LT",
+					Value:     "100",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_lt": 99,
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not LT for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_lt",
+					Operation: "LT",
+					Value:     "80",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_lt": 99,
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+		{
+			name: "test if LTE for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_lte",
+					Operation: "LTE",
+					Value:     "100",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_lte": 99,
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not LTE for INT",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.Int,
+				Condition: &api.Condition{
+					Field:     "test_lte",
+					Operation: "LTE",
+					Value:     "80",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_lte": 99,
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+		{
+			name: "test if PREFIX",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.String,
+				Condition: &api.Condition{
+					Field:     "test_prefix",
+					Operation: "PREFIX",
+					Value:     "test",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_prefix": "test-prefix",
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not PREFIX",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.String,
+				Condition: &api.Condition{
+					Field:     "test_prefix",
+					Operation: "PREFIX",
+					Value:     "test",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_prefix": "tes-prefix",
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+		{
+			name: "test if SUFFIX",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.String,
+				Condition: &api.Condition{
+					Field:     "test_suffix",
+					Operation: "SUFFIX",
+					Value:     "test",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_prefix": "suffix-test",
+			},
+			repo:          "test",
+			envVars:       map[string]string{"PLURAL_TEST_TEST_ITEM": "123"},
+			expectedValue: "123",
+		},
+		{
+			name: "test if not SUFFIX",
+			item: &api.ConfigurationItem{
+				Name: "test_item",
+				Type: bundle.String,
+				Condition: &api.Condition{
+					Field:     "test_suffix",
+					Operation: "SUFFIX",
+					Value:     "test",
+				},
+			},
+			context: &manifest.Context{},
+			ctx: map[string]interface{}{
+				"test_suffix": "suffix-ttt",
+			},
+			repo:          "test",
+			expectedValue: "<nil>",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			for k, v := range test.envVars {
+				os.Setenv(k, v)
+			}
+			defer func(envVars map[string]string) {
+				for k := range envVars {
+					os.Unsetenv(k)
+				}
+			}(test.envVars)
+
+			dir, err := os.MkdirTemp("", "config")
+			assert.NoError(t, err)
+			defer os.RemoveAll(dir)
+
+			err = os.Chdir(dir)
+			assert.NoError(t, err)
+
+			project := manifest.VersionedProjectManifest{
+				Spec: &manifest.ProjectManifest{
+					Project: test.repo,
+				},
+			}
+			out, err := yaml.Marshal(project)
+			assert.NoError(t, err)
+			err = os.WriteFile(filepath.Join(dir, "workspace.yaml"), out, 0644)
+			assert.NoError(t, err)
+
+			err = bundle.Configure(test.ctx, test.item, test.context, test.repo)
+			if test.expectedError != "" {
+				assert.Equal(t, err.Error(), test.expectedError)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			val := test.ctx[test.item.Name]
+			assert.Equal(t, test.expectedValue, fmt.Sprint(val))
+
+		})
+	}
+}
