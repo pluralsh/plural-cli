9d58ec4d8822926be09c830a348eec151e6f74ed
David van der Spek <vanderspek.david@gmail.com>
2023-01-12 12:15:43 +0100

    add some more unit tests
    Signed-off-by: David van der Spek <vanderspek.david@gmail.com>

--- pkg/utils/map_test.go
+++ pkg/utils/map_test.go
@@ -37,15 +37,97 @@ func TestPatchInterfaceMap(t *testing.T) {
 			},
 		},
 		{
-			name: `test if element was changed`,
+			name: `test if new nested element was added`,
 			defaultValues: map[string]map[string]interface{}{
 				"test": {"a": "test", "b": 13},
 			},
 			values: map[string]map[string]interface{}{
-				"test": {"a": "test", "b": 13, "c": "test"},
+				"test": {"a": "test", "b": 13, "c": map[string]interface{}{"d": "e"}},
 			},
 			expectedResult: map[string]map[string]interface{}{
-				"test": {"c": "test"},
+				"test": {"c": map[string]interface{}{"d": "e"}},
+			},
+		},
+		{
+			name: `test if new element was added to nested element`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": map[string]interface{}{"d": "e"}},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": map[string]interface{}{"f": "g"}},
+			},
+			expectedResult: map[string]map[string]interface{}{
+				"test": {"c": map[string]interface{}{"f": "g"}},
+			},
+		},
+		{
+			name: `test if nested element was added changed`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": map[string]interface{}{"d": "e"}},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": map[string]interface{}{"d": "f"}},
+			},
+			expectedResult: map[string]map[string]interface{}{
+				"test": {"c": map[string]interface{}{"d": "f"}},
+			},
+		},
+		{
+			name: `test if element was changed bool true to false`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": true},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"a": "test-2", "b": 14, "c": false},
+			},
+			expectedResult: map[string]map[string]interface{}{
+				"test": {"a": "test-2", "b": float64(14), "c": false},
+			},
+		},
+		{
+			name: `test if element was changed bool false to true`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": false},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"a": "test-2", "b": 14, "c": true},
+			},
+			expectedResult: map[string]map[string]interface{}{
+				"test": {"a": "test-2", "b": float64(14), "c": true},
+			},
+		},
+		{
+			name: `test with equal list element`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": []interface{}{"a", "b"}},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": []interface{}{"a", "b"}},
+			},
+			expectedResult: map[string]map[string]interface{}{},
+		},
+		{
+			name: `test with changed list element`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": []interface{}{"a", "b"}},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": []interface{}{"c", "d"}},
+			},
+			expectedResult: map[string]map[string]interface{}{
+				"test": {"c": []interface{}{"c", "d"}},
+			},
+		},
+		{
+			name: `test with removing element from list`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": []interface{}{"a", "b"}},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": []interface{}{"a"}},
+			},
+			expectedResult: map[string]map[string]interface{}{
+				"test": {"c": []interface{}{"a"}},
 			},
 		},
 		{
@@ -54,6 +136,26 @@ func TestPatchInterfaceMap(t *testing.T) {
 				"test": {"a": "test", "b": 13},
 			},
 			values:         map[string]map[string]interface{}{},
+			expectedResult: map[string]map[string]interface{}{},
+		},
+		{
+			name: `test with empty nested values`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": map[string]interface{}{"d": "test"}},
+			},
+			values: map[string]map[string]interface{}{
+				"test": {"c": map[string]interface{}{}},
+			},
+			expectedResult: map[string]map[string]interface{}{},
+		},
+		{
+			name: `test with empty top level map`,
+			defaultValues: map[string]map[string]interface{}{
+				"test": {"a": "test", "b": 13, "c": map[string]interface{}{"d": "test"}},
+			},
+			values: map[string]map[string]interface{}{
+				"test": map[string]interface{}{},
+			},
 			expectedResult: map[string]map[string]interface{}{},
 		},
 	}
