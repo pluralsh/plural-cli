0b78e5fab871e37ef9216a12bcba9d27ae112c22
michaeljguarino <mguarino46@gmail.com>
2023-02-15 16:40:35 -0500

    Improve crypto unlock
    save the git index to a known place instead of deleting it w/ no hope of recreation

--- cmd/plural/errors.go
+++ cmd/plural/errors.go
@@ -7,4 +7,5 @@ import (
 var (
 	errNoGit      = fmt.Errorf("Could not compare current workspace to origin. Do you have an `origin` remote configured, or does your repo not have an initial commit?")
 	errRemoteDiff = fmt.Errorf("Your local workspace is not in sync with remote. Either `git pull` recent changes or `git push` any missed changes.")
+	errUnlock     = fmt.Errorf("could not decrypt your repo, this is likely due to using the wrong key at ~/.plural/key. The original key might be in a backup or on your previous machine.")
 )
