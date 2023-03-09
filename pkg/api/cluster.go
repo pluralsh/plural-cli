95d2a617a8a195d04a8f947d9da302d15ec7384a
michaeljguarino <mguarino46@gmail.com>
2023-03-09 02:11:49 -0500

    Add cluster promotion commands (#361)
    * Add cluster promotion commands
    
    These commands will allow users to perform all the main CRUD for cluster promotions
    
    * clean up file input

--- pkg/api/cluster.go
+++ pkg/api/cluster.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/pluralsh/gqlclient"
+	"github.com/samber/lo"
 )
 
 func (client *client) DestroyCluster(domain, name, provider string) error {
@@ -11,4 +12,70 @@ func (client *client) DestroyCluster(domain, name, provider string) error {
 	}
 
 	return nil
+}
+
+func (client *client) CreateDependency(source, dest string) error {
+	_, err := client.pluralClient.CreateDependency(client.ctx, source, dest)
+	return err
+}
+
+func (client *client) PromoteCluster() error {
+	_, err := client.pluralClient.PromoteCluster(client.ctx)
+	return err
+}
+
+func (client *client) Clusters() ([]*Cluster, error) {
+	resp, err := client.pluralClient.Clusters(client.ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	clusters := make([]*Cluster, 0)
+	for _, edge := range resp.Clusters.Edges {
+		node := edge.Node
+		clusters = append(clusters, &Cluster{
+			Id:       node.ID,
+			Name:     node.Name,
+			Provider: string(node.Provider),
+			GitUrl:   lo.FromPtr(node.GitURL),
+			Owner: &User{
+				Id:    node.Owner.ID,
+				Name:  node.Owner.Name,
+				Email: node.Owner.Email,
+			},
+		})
+	}
+
+	return clusters, nil
+}
+
+func (client *client) Cluster(id string) (*Cluster, error) {
+	resp, err := client.pluralClient.ClusterInfo(client.ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	node := resp.Cluster
+	upgradeInfo := make([]*UpgradeInfo, 0)
+	for _, info := range node.UpgradeInfo {
+		upgradeInfo = append(upgradeInfo, &UpgradeInfo{
+			Count: lo.FromPtr(info.Count),
+			Installation: &Installation{
+				Repository: convertRepository(info.Installation.Repository),
+			},
+		})
+	}
+
+	cluster := &Cluster{
+		Id:       node.ID,
+		Name:     node.Name,
+		Provider: string(node.Provider),
+		GitUrl:   lo.FromPtr(node.GitURL),
+		Owner: &User{
+			Id:    node.Owner.ID,
+			Name:  node.Owner.Name,
+			Email: node.Owner.Email,
+		},
+		UpgradeInfo: upgradeInfo,
+	}
+	return cluster, nil
 }
