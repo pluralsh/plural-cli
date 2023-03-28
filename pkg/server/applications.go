8cc72ba850b4311481d07d15513849ece19a0195
michaeljguarino <mguarino46@gmail.com>
2023-03-28 02:29:03 -0400

    Add server endpoint to list applications (#379)
    Will be used for cloud shell installer health indicators, and maybe local installer

---
+++ pkg/server/applications.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pluralsh/plural/pkg/application"
+	"github.com/pluralsh/plural/pkg/kubernetes"
+)
+
+func listApplications(c *gin.Context) error {
+	kubeConf, err := kubernetes.KubeConfig()
+	if err != nil {
+		return err
+	}
+
+	apps, err := application.ListAll(kubeConf)
+	if err != nil {
+		return err
+	}
+
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, apps)
+	return nil
+}
