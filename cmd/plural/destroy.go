ef2a3ff381522b5f538d33af8de6f07009f6f16a
Marcin Maciaszczyk <marcin@plural.sh>
2023-08-01 14:30:25 +0200

    Refactor destroy steps

--- cmd/plural/destroy.go
+++ cmd/plural/destroy.go
@@ -39,90 +39,68 @@ func clusterAPIDestroySteps(path string, destroy func() error) []*Step {
 	pm, _ := manifest.FetchProject()
 	homedir, _ := os.UserHomeDir()
 	sanitizedPath := pathing.SanitizeFilepath(path)
-	providerBootstrapFlags := []string{}
+	providerBootstrapFlags := getProviderBootstrapFlags(pm.Provider)
 	prov, _ := provider.GetProvider()
 	clusterKubeContext := prov.KubeContext()
 
-	runDestroy := func(_ []string) error {
-		return destroy()
-	}
-
-	switch pm.Provider {
-	case "aws":
-		providerBootstrapFlags = []string{
-			"--set", "cluster-api-provider-aws.cluster-api-provider-aws.bootstrapMode=true",
-			"--set", "bootstrap.aws-ebs-csi-driver.enabled=false",
-			"--set", "bootstrap.aws-load-balancer-controller.enabled=false",
-			"--set", "bootstrap.cluster-autoscaler.enabled=false",
-			"--set", "bootstrap.metrics-server.enabled=false",
-			"--set", "bootstrap.snapshot-controller.enabled=false",
-			"--set", "bootstrap.snapshot-validation-webhook.enabled=false",
-			"--set", "bootstrap.tigera-operator.enabled=false",
-		}
-	case "azure":
-		providerBootstrapFlags = []string{}
-	case "gcp":
-		providerBootstrapFlags = []string{}
-	case "google":
-		providerBootstrapFlags = []string{}
-	}
-
 	return []*Step{
 		{
-			Name:       "create bootstrap cluster",
+			Name:       "Create local bootstrap cluster",
 			Args:       []string{"plural", "bootstrap", "cluster", "create", "bootstrap", "--skip-if-exists"},
 			TargetPath: sanitizedPath,
 			Execute:    RunPlural,
 		},
 		{
-			Name:       "bootstrap crds",
+			Name:       "Bootstrap CRDs in local cluster",
 			Args:       []string{"plural", "--bootstrap", "wkspace", "crds", "bootstrap"},
 			TargetPath: sanitizedPath,
 			Execute:    RunPlural,
 		},
 		{
-			Name:       "install capi operators",
+			Name:       "Install Cluster API operators in local cluster",
 			Args:       append([]string{"plural", "--bootstrap", "wkspace", "helm", "bootstrap", "--skip", "cluster-api-cluster"}, providerBootstrapFlags...),
 			TargetPath: sanitizedPath,
 			Execute:    RunPlural,
 		},
 		{
-			Name:       "move",
+			Name:       "Move resources from target to local cluster",
 			Args:       []string{"plural", "bootstrap", "cluster", "move", "--kubeconfig-context", clusterKubeContext, "--to-kubeconfig", pathing.SanitizeFilepath(filepath.Join(homedir, ".kube", "config")), "--to-kubeconfig-context", "kind-bootstrap"},
 			Execute:    RunPlural,
 			TargetPath: sanitizedPath,
 		},
 		{
-			Name:       "destroy bootstrap on target cluster",
+			Name:       "Destroy bootstrap on target cluster",
 			TargetPath: sanitizedPath,
-			Execute:    runDestroy,
+			Execute: func(_ []string) error {
+				return destroy()
+			},
 		},
 		{
-			Name:       "wait for cluster",
+			Name:       "Wait for cluster",
 			Args:       []string{"plural", "--bootstrap", "clusters", "wait", "bootstrap", pm.Cluster},
 			Execute:    RunPlural,
 			TargetPath: sanitizedPath,
 		},
 		{
-			Name:       "wait for machines running",
+			Name:       "Wait for machine pools",
 			Args:       []string{"plural", "--bootstrap", "clusters", "mpwait", "bootstrap", pm.Cluster},
 			Execute:    RunPlural,
 			TargetPath: sanitizedPath,
 		},
 		{
-			Name:       "cleanup cluster resources",
+			Name:       "Cleanup cluster resources",
 			Args:       nil,
 			TargetPath: sanitizedPath,
 			Execute:    CleanupClusterResources,
 		},
 		{
-			Name:       "destroy cluster API",
+			Name:       "Destroy cluster API",
 			Args:       []string{"plural", "bootstrap", "cluster", "destroy-cluster-api", pm.Cluster},
 			Execute:    RunPlural,
 			TargetPath: sanitizedPath,
 		},
 		{
-			Name:       "destroy kind cluster",
+			Name:       "Destroy local cluster",
 			Args:       []string{"plural", "--bootstrap", "bootstrap", "cluster", "delete", "bootstrap"},
 			Execute:    RunPlural,
 			TargetPath: sanitizedPath,
