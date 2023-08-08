753103e86ca8d46065b0d5a77c91eb28af00e47c
michaeljguarino <mguarino46@gmail.com>
2023-08-08 14:45:26 -0400

    Implement install version command
    Adds `packages install` and `packages show terraform|helm` to support specific named version installs to make it easier to toggle versions.

--- cmd/plural/packages.go
+++ cmd/plural/packages.go
@@ -14,9 +14,15 @@ import (
 func (p *Plural) packagesCommands() []cli.Command {
 	return []cli.Command{
 		{
+			Name:      "install",
+			Usage:     "installs a package at a specific version",
+			ArgsUsage: "helm|terraform REPO NAME VSN",
+			Action:    affirmed(requireArgs(p.installPackage, []string{"TYPE", "REPO", "NAME", "VERSION"}), "Are you sure you want to install this package?", "PLURAL_PACKAGES_INSTALL"),
+		},
+		{
 			Name:      "uninstall",
 			Usage:     "uninstall a helm or terraform package",
-			ArgsUsage: "TYPE REPO NAME",
+			ArgsUsage: "helm|terraform REPO NAME",
 			Action:    latestVersion(affirmed(requireArgs(rooted(p.uninstallPackage), []string{"TYPE", "REPO", "NAME"}), "Are you sure you want to uninstall this package?", "PLURAL_PACKAGES_UNINSTALL")),
 		},
 		{
@@ -25,7 +31,85 @@ func (p *Plural) packagesCommands() []cli.Command {
 			ArgsUsage: "REPO",
 			Action:    latestVersion(requireArgs(rooted(p.listPackages), []string{"REPO"})),
 		},
+		{
+			Name:        "show",
+			Usage:       "Shows version information for packages within a plural repo",
+			Subcommands: p.showCommands(),
+		},
 	}
+}
+
+func (p *Plural) showCommands() []cli.Command {
+	return []cli.Command{
+		{
+			Name:      "helm",
+			Usage:     "list versions for a helm chart",
+			ArgsUsage: "REPO NAME",
+			Action:    requireArgs(p.showHelm, []string{"REPO", "NAME"}),
+		},
+		{
+			Name:      "terraform",
+			Usage:     "list versions for a terraform module",
+			ArgsUsage: "REPO NAME",
+			Action:    requireArgs(p.showTerraform, []string{"REPO", "NAME"}),
+		},
+	}
+}
+
+func (p *Plural) installPackage(c *cli.Context) error {
+	p.InitPluralClient()
+	tp, repo, name, vsn := c.Args().Get(0), c.Args().Get(1), c.Args().Get(2), c.Args().Get(3)
+	if err := p.Client.InstallVersion(tp, repo, name, vsn); err != nil {
+		return err
+	}
+
+	utils.Success("Successfully installed %s %s version %s in %s\n", tp, name, vsn, repo)
+	utils.Highlight("To apply the module in your cluster, you'll need to run `plural build --only %s && plural deploy", repo)
+	return nil
+}
+
+func (p *Plural) showHelm(c *cli.Context) error {
+	p.InitPluralClient()
+	repo, name := c.Args().Get(0), c.Args().Get(1)
+	chart, err := api.FindChart(p.Client, repo, name)
+	if err != nil {
+		return err
+	}
+
+	vsns, err := p.Client.GetVersions(chart.Id)
+	if err != nil {
+		return err
+	}
+
+	header := []string{"Name", "Version", "App Version", "Created"}
+	return utils.PrintTable(vsns, header, func(vsn *api.Version) ([]string, error) {
+		appVsn := ""
+		if app, ok := vsn.Helm["appVersion"]; ok {
+			if v, ok := app.(string); ok {
+				appVsn = v
+			}
+		}
+		return []string{chart.Name, vsn.Version, appVsn, vsn.InsertedAt}, nil
+	})
+}
+
+func (p *Plural) showTerraform(c *cli.Context) error {
+	p.InitPluralClient()
+	repo, name := c.Args().Get(0), c.Args().Get(1)
+	chart, err := api.FindTerraform(p.Client, repo, name)
+	if err != nil {
+		return err
+	}
+
+	vsns, err := p.Client.GetTerraformVersions(chart.Id)
+	if err != nil {
+		return err
+	}
+
+	header := []string{"Name", "Version", "Created"}
+	return utils.PrintTable(vsns, header, func(vsn *api.Version) ([]string, error) {
+		return []string{chart.Name, vsn.Version, vsn.InsertedAt}, nil
+	})
 }
 
 func (p *Plural) listPackages(c *cli.Context) error {
