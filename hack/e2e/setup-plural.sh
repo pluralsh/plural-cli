44dc4e9bbe3f540ddf831e33f0908ae293ddf2cd
Lukasz Zajaczkowski <zreigz@gmail.com>
2023-08-18 10:25:06 +0200

    distinguish between regular and cluster api - fix

--- hack/e2e/setup-plural.sh
+++ hack/e2e/setup-plural.sh
@@ -67,7 +67,7 @@ metadata:
   name: testcli
 spec:
   cluster: testcli
-  clusterapi: "$USE_CLUSTER_API"
+  clusterapi: false
   bucket: testcli-tf-state
   project: ""
   provider: kind
@@ -80,7 +80,28 @@ spec:
   bucketPrefix: test
   context: {}
 EOF
-
+if [ "USE_CLUSTER_API" == "true" ]; then
+cat << EOF > "$TESTDIR"/workspace.yaml
+apiVersion: plural.sh/v1alpha1
+kind: ProjectManifest
+metadata:
+  name: testcli
+spec:
+  cluster: testcli
+  clusterapi: true
+  bucket: testcli-tf-state
+  project: ""
+  provider: kind
+  region: us-east-1
+  owner:
+    email: cli-e2e@pluraldev.sh
+  network:
+    subdomain: clie2e.onplural.sh
+    pluraldns: true
+  bucketPrefix: test
+  context: {}
+EOF
+fi
 
 echodate "Entering to work directory ..."
 cd "$TESTDIR"
