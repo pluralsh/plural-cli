78823520faaec841e574120b29e9eb993421b889
Lukasz Zajaczkowski <zreigz@gmail.com>
2023-08-18 12:59:19 +0200

    add e2e test to check installed packages

--- pkg/test/e2eclusterapi/e2e_api_test.go
+++ pkg/test/e2eclusterapi/e2e_api_test.go
@@ -4,7 +4,9 @@ package e2e_test
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"testing"
 
@@ -32,4 +34,31 @@ func TestApiListInstallations(t *testing.T) {
 	expected := []string{"bootstrap"}
 	expects := containers.ToSet(expected)
 	assert.True(t, expects.Equal(containers.ToSet(installations)), fmt.Sprintf("the expected %s is different then %s", expected, installations))
+}
+
+func TestPackagesList(t *testing.T) {
+	homeDir, err := os.UserHomeDir()
+	assert.NoError(t, err)
+
+	testDir := path.Join(homeDir, "test")
+
+	err = os.Chdir(testDir)
+	assert.NoError(t, err)
+	cmd := exec.Command("plural", "packages", "list", "bootstrap")
+	cmdOutput, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	packages := make([]string, 0)
+	rows := strings.Split(string(cmdOutput[:]), "\n")
+	for _, row := range rows[3:] { // Skip the heading row and iterate through the remaining rows
+		row = strings.ReplaceAll(row, "|", "")
+		cols := strings.Fields(row) // Extract each column from the row.
+		if len(cols) == 3 {
+			packages = append(packages, cols[1])
+		}
+	}
+	expected := []string{"bootstrap", "kind-bootstrap-cluster-api", "cluster-api-control-plane", "cluster-api-bootstrap", "plural-certmanager-webhook", "cluster-api-cluster", "cluster-api-core", "cluster-api-provider-docker"}
+	expects := containers.ToSet(expected)
+	assert.True(t, expects.Equal(containers.ToSet(packages)), fmt.Sprintf("the expected %s is different then %s", expected, packages))
 }
