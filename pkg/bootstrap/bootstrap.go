f26fe600e0db7b0827061feb790e2d427bf59a89
Marcin Maciaszczyk <marcin@plural.sh>
2023-08-30 11:45:55 +0200

    Extract methods from bootstrap steps

--- pkg/bootstrap/bootstrap.go
+++ pkg/bootstrap/bootstrap.go
@@ -12,9 +12,119 @@ import (
 	"github.com/pluralsh/plural/pkg/utils"
 )
 
+// deleteBootstrapCluster executes single step to destroy local cluster.
+func deleteBootstrapCluster(runPlural ActionFunc) {
+	if err := ExecuteSteps([]*Step{{
+		Name:    "Destroy local cluster",
+		Args:    []string{"plural", "--bootstrap", "bootstrap", "cluster", "delete", "bootstrap"},
+		Execute: runPlural,
+	}}); err != nil {
+		utils.Error("%s", err)
+	}
+}
+
+func installStorageClass(_ []string) error {
+	kube, err := kubernetes.Kubernetes()
+	if err != nil {
+		return err
+	}
+
+	f, err := os.CreateTemp("", "storageClass")
+	if err != nil {
+		return err
+	}
+	defer func(name string) {
+		err := os.Remove(name)
+		if err != nil {
+			utils.Error("%s", err)
+		}
+	}(f.Name())
+
+	_, err = f.WriteString(storageClassManifest)
+	if err != nil {
+		return err
+	}
+
+	return kube.Apply(f.Name(), true)
+}
+
+// saveKindKubeconfig exports kind kubeconfig to file.
+func saveKindKubeconfig(_ []string) error {
+	man, err := manifest.FetchProject()
+	if err != nil {
+		return err
+	}
+
+	bootstrapPath, err := GetBootstrapPath()
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("kind", "export", "kubeconfig", "--name", man.Cluster,
+		"--kubeconfig", filepath.Join(bootstrapPath, "terraform", "kube_config_cluster.yaml"))
+	return utils.Execute(cmd)
+}
+
+func enableAzureOIDCIssuer(_ []string) error {
+	man, err := manifest.FetchProject()
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("az", "aks", "update", "-g", man.Project, "-n", man.Cluster, "--enable-oidc-issuer")
+	return utils.Execute(cmd)
+}
+
+func removeHelmSecrets(_ []string) error {
+	cmd := exec.Command("kubectl", "delete", "secret", "-n", "bootstrap", "-l", "owner=helm")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func moveHelmSecrets(_ []string) error {
+	getCmd := exec.Command("kubectl", "get", "secret", "-n", "bootstrap", "-l", "owner=helm", "-o", "yaml", "--context", "kind-bootstrap")
+	createCmd := exec.Command("kubectl", "create", "-f", "-")
+
+	r, w := io.Pipe()
+	getCmd.Stdout = w
+	getCmd.Stderr = os.Stderr
+	createCmd.Stdin = r
+	createCmd.Stdout = os.Stdout
+	createCmd.Stderr = os.Stderr
+
+	err := getCmd.Start()
+	if err != nil {
+		return err
+	}
+
+	err = createCmd.Start()
+	if err != nil {
+		return err
+	}
+
+	err = getCmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	err = createCmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
 // getBootstrapSteps returns list of steps to run during cluster bootstrap.
 func getBootstrapSteps(runPlural ActionFunc, additionalFlags []string) ([]*Step, error) {
-	projectManifest, err := manifest.FetchProject()
+	man, err := manifest.FetchProject()
 	if err != nil {
 		return nil, err
 	}
@@ -24,7 +134,7 @@ func getBootstrapSteps(runPlural ActionFunc, additionalFlags []string) ([]*Step,
 		return nil, err
 	}
 
-	flags := append(getBootstrapFlags(projectManifest.Provider), additionalFlags...)
+	flags := append(getBootstrapFlags(man.Provider), additionalFlags...)
 
 	var steps []*Step
 	steps = append(steps, []*Step{
@@ -50,56 +160,26 @@ func getBootstrapSteps(runPlural ActionFunc, additionalFlags []string) ([]*Step,
 		},
 		{
 			Name:    "Wait for cluster",
-			Args:    []string{"plural", "--bootstrap", "clusters", "wait", "bootstrap", projectManifest.Cluster},
+			Args:    []string{"plural", "--bootstrap", "clusters", "wait", "bootstrap", man.Cluster},
 			Execute: runPlural,
 		},
 	}...)
 
-	if projectManifest.Provider == provider.KIND {
+	if man.Provider == provider.KIND {
 		steps = append(steps, []*Step{
 			{
 				Name: "Install Network",
 				Execute: func(_ []string) error {
-					return InstallCilium(projectManifest.Cluster)
+					return InstallCilium(man.Cluster)
 				},
 			},
 			{
-				Name: "Install StorageClass",
-				Execute: func(_ []string) error {
-					kube, err := kubernetes.Kubernetes()
-					if err != nil {
-						return err
-					}
-					f, err := os.CreateTemp("", "storageClass")
-					if err != nil {
-						return err
-					}
-					defer os.Remove(f.Name())
-					_, err = f.WriteString(storageClassManifest)
-					if err != nil {
-						return err
-					}
-					if err := kube.Apply(f.Name(), true); err != nil {
-						return err
-					}
-
-					return nil
-				},
+				Name:    "Install StorageClass",
+				Execute: installStorageClass,
 			},
 			{
-				Name: "Save kubeconfig",
-				Execute: func(_ []string) error {
-					bootstrapPath, err := GetBootstrapPath()
-					if err != nil {
-						return err
-					}
-					cmd := exec.Command("kind", "export", "kubeconfig", "--name", projectManifest.Cluster, "--kubeconfig", filepath.Join(bootstrapPath, "terraform", "kube_config_cluster.yaml"))
-					if err := utils.Execute(cmd); err != nil {
-						return err
-					}
-
-					return nil
-				},
+				Name:    "Save kubeconfig",
+				Execute: saveKindKubeconfig,
 			},
 		}...)
 	}
@@ -107,7 +187,7 @@ func getBootstrapSteps(runPlural ActionFunc, additionalFlags []string) ([]*Step,
 	steps = append(steps, []*Step{
 		{
 			Name:    "Wait for machine pools",
-			Args:    []string{"plural", "--bootstrap", "clusters", "mpwait", "bootstrap", projectManifest.Cluster},
+			Args:    []string{"plural", "--bootstrap", "clusters", "mpwait", "bootstrap", man.Cluster},
 			Execute: runPlural,
 		},
 	}...)
@@ -115,18 +195,11 @@ func getBootstrapSteps(runPlural ActionFunc, additionalFlags []string) ([]*Step,
 	// TODO:
 	//  Once https://github.com/kubernetes-sigs/cluster-api-provider-azure/issues/2498
 	//  will be done we can use it and remove this step.
-	if projectManifest.Provider == provider.AZURE {
+	if man.Provider == provider.AZURE {
 		steps = append(steps, []*Step{
 			{
-				Name: "Enable OIDC issuer",
-				Execute: func(_ []string) error {
-					cmd := exec.Command("az", "aks", "update", "-g", projectManifest.Project, "-n", projectManifest.Cluster, "--enable-oidc-issuer")
-					if err := utils.Execute(cmd); err != nil {
-						return err
-					}
-
-					return nil
-				},
+				Name:    "Enable OIDC issuer",
+				Execute: enableAzureOIDCIssuer,
 			},
 		}...)
 	}
@@ -169,55 +242,12 @@ func getBootstrapSteps(runPlural ActionFunc, additionalFlags []string) ([]*Step,
 			Execute: runPlural,
 		},
 		{
-			Name: "Remove Helm secrets",
-			Execute: func(arguments []string) error {
-				cmd := exec.Command("kubectl", "delete", "secret", "-n", "bootstrap", "-l", "owner=helm")
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-
-				return cmd.Run()
-			},
+			Name:    "Remove Helm secrets",
+			Execute: removeHelmSecrets,
 		},
 		{
-			Name: "Copy Helm secrets",
-			Execute: func(arguments []string) error {
-				getCmd := exec.Command("kubectl", "get", "secret", "-n", "bootstrap", "-l", "owner=helm", "-o", "yaml", "--context", "kind-bootstrap")
-				createCmd := exec.Command("kubectl", "create", "-f", "-")
-
-				r, w := io.Pipe()
-				getCmd.Stdout = w
-				getCmd.Stderr = os.Stderr
-				createCmd.Stdin = r
-				createCmd.Stdout = os.Stdout
-				createCmd.Stderr = os.Stderr
-
-				err := getCmd.Start()
-				if err != nil {
-					return err
-				}
-
-				err = createCmd.Start()
-				if err != nil {
-					return err
-				}
-
-				err = getCmd.Wait()
-				if err != nil {
-					return err
-				}
-
-				err = w.Close()
-				if err != nil {
-					return err
-				}
-
-				err = createCmd.Wait()
-				if err != nil {
-					return err
-				}
-
-				return err
-			},
+			Name:    "Move Helm secrets",
+			Execute: moveHelmSecrets,
 		},
 		{
 			Name:    "Destroy local cluster",
@@ -251,14 +281,4 @@ func BootstrapCluster(runPlural ActionFunc) error {
 
 	utils.Success("Cluster bootstrapped successfully!\n")
 	return nil
-}
-
-func deleteBootstrapCluster(runPlural ActionFunc) {
-	if err := ExecuteSteps([]*Step{{
-		Name:    "Destroy local cluster",
-		Args:    []string{"plural", "--bootstrap", "bootstrap", "cluster", "delete", "bootstrap"},
-		Execute: runPlural,
-	}}); err != nil {
-		utils.Error("%s", err)
-	}
 }
