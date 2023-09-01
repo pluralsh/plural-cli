1aebab9db8a2667400e47ba11aa4c27ba90261e3
michaeljguarino <mguarino46@gmail.com>
2023-09-01 14:20:39 -0400

    Try using abduco for session management which should have buffering (#439)
    * Try using abduco for session management which should have buffering
    
    dtach does work but is too primitive to buffer and repaint previous output.  Abduco might be a better solve
    
    * add annoying tput command
    
    * try force enabling scroll
    
    * different use of tput rmcap

--- boot.sh
+++ boot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+tput rmcup
 
 if [ -f /home/plural/.env ]; then
   source /home/plural/.env
 fi
 
 cd ~/workspace || echo "could not check out workspace repo, ensure it exists and git permissions are correct"
-
 zsh
