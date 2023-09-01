32a8f88a26645d5a5293a62d435a9f0ebebf9b5b
michaeljguarino <mguarino46@gmail.com>
2023-09-01 14:20:39 -0400

    Try using abduco for session management which should have buffering (#439)
    * Try using abduco for session management which should have buffering
    
    dtach does work but is too primitive to buffer and repaint previous output.  Abduco might be a better solve
    
    * add annoying tput command
    
    * try force enabling scroll
    
    * different use of tput rmcap

--- start-session.sh
+++ start-session.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-session="/tmp/plural-workspace"
+session="plural-workspace"
 
 # ensure necessary env vars are populated
 if [ -f /home/plural/.env ]; then
   source /home/plural/.env
 fi
 
-dtach -A $session -r winch -Ez /home/plural/boot.sh
+abduco -A $session /home/plural/boot.sh
