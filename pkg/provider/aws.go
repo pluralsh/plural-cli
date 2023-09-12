7f60754ce8f3cb741f5084d99adefb4fbc8dc853
Lukasz Zajaczkowski <zreigz@gmail.com>
2023-09-12 12:18:03 +0200

    bump migrator

--- pkg/provider/aws.go
+++ pkg/provider/aws.go
@@ -134,12 +134,8 @@ func awsFromManifest(man *manifest.ProjectManifest) (*AWSProvider, error) {
 	if err != nil {
 		return nil, err
 	}
-	accountId, err := GetAwsAccount(ctx)
-	if err != nil {
-		return nil, err
-	}
 	providerCtx := map[string]interface{}{}
-	providerCtx["AWSAccountID"] = accountId
+
 	return &AWSProvider{Clus: man.Cluster, project: man.Project, bucket: man.Bucket, Reg: man.Region, storageClient: client, goContext: &ctx, ctx: providerCtx}, nil
 }
 
