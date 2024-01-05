209877dfcf555b5862c3642afd437cb3e9a292bb
Hans Rauer <h.w.rauer@googlemail.com>
2024-01-05 12:19:08 +0100

    pretty print for debug

--- cmd/plural/cd_pipelines.go
+++ cmd/plural/cd_pipelines.go
@@ -1,8 +1,11 @@
 package plural
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"reflect"
+	"strings"
 
 	"github.com/pluralsh/plural-cli/pkg/console"
 	"github.com/pluralsh/plural-cli/pkg/utils"
@@ -52,14 +55,57 @@ func (p *Plural) handleCreatePipeline(c *cli.Context) error {
 
 	name, attrs, err := console.ConstructPipelineInput(bytes)
 	if err != nil {
+		fmt.Printf("Error constructing pipeline input: %v\n", err)
 		return err
 	}
 
+	fmt.Printf("Pipeline name: %s\n", name)
+	fmt.Printf("Pipeline attributes: %+v\n", attrs)
+	PrettyPrintStruct(attrs, 3)
+
 	pipe, err := p.ConsoleClient.SavePipeline(name, *attrs)
 	if err != nil {
+		fmt.Printf("Error saving pipeline: %v\n", err)
 		return err
 	}
 
 	utils.Success("Pipeline %s created successfully\n", pipe.Name)
 	return nil
+}
+
+func PrettyPrintStruct(v interface{}, depth int) {
+	rv := reflect.ValueOf(v)
+	if rv.Kind() == reflect.Ptr && !rv.IsNil() {
+		PrettyPrintStruct(rv.Elem().Interface(), depth)
+		return
+	}
+
+	if rv.Kind() != reflect.Struct {
+		fmt.Printf("%s%v\n", strings.Repeat("  ", depth), rv.Interface())
+		return
+	}
+
+	for i := 0; i < rv.NumField(); i++ {
+		field := rv.Field(i)
+		fieldType := rv.Type().Field(i)
+		fmt.Printf("%s%s: ", strings.Repeat("  ", depth), fieldType.Name)
+
+		if field.Kind() == reflect.Struct {
+			fmt.Println()
+			PrettyPrintStruct(field.Interface(), depth+1)
+		} else if field.Kind() == reflect.Ptr {
+			if field.IsNil() {
+				fmt.Println("nil")
+			} else {
+				PrettyPrintStruct(field.Elem().Interface(), depth+1)
+			}
+		} else if field.Kind() == reflect.Slice || field.Kind() == reflect.Array {
+			fmt.Println()
+			for j := 0; j < field.Len(); j++ {
+				PrettyPrintStruct(field.Index(j).Interface(), depth+1)
+			}
+		} else {
+			fmt.Println(field.Interface())
+		}
+	}
 }
