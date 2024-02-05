391a7697f5146ff15927ff08583d4b7b216423a2
michaeljguarino <mguarino46@gmail.com>
2024-02-05 16:32:01 -0500

    Create-mode PR templating (#487)
    * Create-mode PR templating
    
    Adds the ability to create new files to a pr template, sourced either in-repo or externally
    
    * add more goreleaser release targets

--- pkg/pr/creates.go
+++ pkg/pr/creates.go
@@ -1,5 +1,26 @@
 package pr
 
+import (
+	"path/filepath"
+)
+
 func applyCreates(creates *CreateSpec, ctx map[string]interface{}) error {
+	if creates == nil {
+		return nil
+	}
+
+	for _, tpl := range creates.Templates {
+		source := tpl.Source
+		if tpl.External {
+			source = filepath.Join(creates.ExternalDir, source)
+		}
+
+		if err := replaceTo(source, tpl.Destination, func(data []byte) ([]byte, error) {
+			return templateReplacement(data, ctx)
+		}); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
