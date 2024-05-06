9b031cede59bdada6ef45cdee7f034b64f2f19e0
michaeljguarino <mguarino46@gmail.com>
2024-05-06 08:54:36 -0400

    Add `plural cd services template` (#509)
    This will make it a much smoother experience to test .liquid or .tpl files as they are being developed.  Should be functionally equivalent to the process done in-agent.

---
+++ pkg/cd/template/render.go
@@ -0,0 +1,41 @@
+package template
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	console "github.com/pluralsh/console-client-go"
+	"github.com/pluralsh/polly/template"
+)
+
+func RenderYaml(path string, bindings map[string]interface{}) ([]byte, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if strings.HasSuffix(path, ".tpl") {
+		return template.RenderTpl(content, bindings)
+	}
+
+	if strings.HasSuffix(path, ".liquid") {
+		return template.RenderLiquid(content, bindings)
+	}
+
+	return content, fmt.Errorf("Not a .liquid or .tpl file")
+}
+
+func RenderService(path string, svc *console.ServiceDeploymentExtended) ([]byte, error) {
+	bindings := map[string]interface{}{
+		"Configuration": configMap(svc),
+		"Cluster":       clusterConfiguration(svc.Cluster),
+		"Contexts":      contexts(svc),
+	}
+
+	for k, v := range bindings {
+		bindings[strings.ToLower(k)] = v
+	}
+
+	return RenderYaml(path, bindings)
+}
