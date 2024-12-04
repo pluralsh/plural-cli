d4c408c3b074c2fe7d0cb7c228d4108ab138c3f4
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-12-04 13:35:55 +0100

    remove stack commands

--- cmd/command/plural/plural.go
+++ cmd/command/plural/plural.go
@@ -26,7 +26,6 @@ import (
 	"github.com/pluralsh/plural-cli/cmd/command/proxy"
 	"github.com/pluralsh/plural-cli/cmd/command/push"
 	"github.com/pluralsh/plural-cli/cmd/command/repo"
-	"github.com/pluralsh/plural-cli/cmd/command/stack"
 	"github.com/pluralsh/plural-cli/cmd/command/up"
 	"github.com/pluralsh/plural-cli/cmd/command/upgrade"
 	"github.com/pluralsh/plural-cli/cmd/command/version"
@@ -244,7 +243,6 @@ func CreateNewApp(plural *Plural) *cli.App {
 		push.Command(plural.Plural),
 		repo.Command(plural.Plural),
 		repo.APICommand(plural.Plural),
-		stack.Command(plural.Plural),
 		log.Command(plural.Plural),
 		info.Command(plural.Plural),
 		cmdinit.Command(plural.Plural),
