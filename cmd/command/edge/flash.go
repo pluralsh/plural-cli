b19e97e45883406fe1462f2188d27ea1b128fbe0
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-02-03 12:17:20 +0100

    add progress bar

--- cmd/command/edge/flash.go
+++ cmd/command/edge/flash.go
@@ -6,13 +6,13 @@ import (
 	"os"
 
 	"github.com/pluralsh/plural-cli/pkg/utils"
+	"github.com/schollz/progressbar/v3"
 	"github.com/urfave/cli"
 )
 
 func (p *Plural) handleEdgeFlash(c *cli.Context) error {
 	imagePath := c.String("image")
 	devicePath := c.String("device")
-	blockSize := c.Int("block-size")
 
 	in, err := os.Open(imagePath)
 	if err != nil {
@@ -20,25 +20,20 @@ func (p *Plural) handleEdgeFlash(c *cli.Context) error {
 	}
 	defer in.Close()
 
+	stat, err := os.Stat(imagePath)
+	if err != nil {
+		return err
+	}
+
 	out, err := os.OpenFile(devicePath, os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		return fmt.Errorf("could not open device: %v", err)
 	}
 	defer out.Close()
 
-	buf := make([]byte, blockSize)
-	for {
-		n, err := in.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return fmt.Errorf("could not read image: %v", err)
-		}
-		_, err = out.Write(buf[:n])
-		if err != nil {
-			return fmt.Errorf("could not write image: %v", err)
-		}
+	bar := progressbar.DefaultBytes(stat.Size(), "flashing")
+	if _, err = io.Copy(io.MultiWriter(out, bar), in); err != nil {
+		return err
 	}
 
 	utils.Success("image flashed on %s device\n", devicePath)
