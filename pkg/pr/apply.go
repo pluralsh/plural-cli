842ea58ac440b4d78f4dc3308cc2ac04daedc44b
michaeljguarino <mguarino46@gmail.com>
2025-07-30 11:44:49 -0400

    Apply creates before updates (#658)
    I'm not sure why the ordering was the other way around tbh, but this allows you to create then overlay a file in one pra

--- pkg/pr/apply.go
+++ pkg/pr/apply.go
@@ -1,11 +1,11 @@
 package pr
 
 func Apply(template *PrTemplate) error {
-	if err := applyUpdates(template.Spec.Updates, template.Context); err != nil {
+	if err := applyCreates(template.Spec.Creates, template.Context); err != nil {
 		return err
 	}
 
-	if err := applyCreates(template.Spec.Creates, template.Context); err != nil {
+	if err := applyUpdates(template.Spec.Updates, template.Context); err != nil {
 		return err
 	}
 
