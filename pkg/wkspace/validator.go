bd008d772d507a4a7d7bf98b8e520a11d37711ab
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-11-03 14:29:33 +0100

    add Git SSH preflight checks

--- pkg/wkspace/validator.go
+++ pkg/wkspace/validator.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/pluralsh/plural-cli/pkg/executor"
 	"github.com/pluralsh/plural-cli/pkg/utils"
+	"github.com/pluralsh/plural-cli/pkg/utils/git"
 )
 
 func Preflight() (bool, error) {
@@ -17,6 +19,7 @@ func Preflight() (bool, error) {
 			return true, utils.HighlightError(fmt.Errorf("%s not installed", req))
 		}
 	}
+
 	fmt.Print("\nTesting if git ssh is properly configured...\n")
 	if err := checkGitSSH(); err != nil {
 		fmt.Printf("%s\n\n", err.Error())
@@ -38,6 +41,18 @@ func Preflight() (bool, error) {
 	cmd = exec.Command("git", "ls-remote", "--exit-code")
 	if _, err := cmd.CombinedOutput(); err != nil {
 		return true, utils.HighlightError(fmt.Errorf("repository has no remotes set, make sure that at least one remote is set"))
+	}
+
+	url, err := git.GetURL()
+	if err != nil {
+		return true, err
+	}
+
+	if strings.HasPrefix(url, "http") {
+		utils.Error("Found non-ssh upstream url %s, please reclone the repo with SSH and retry.\n", url)
+		utils.Warn("Please ensure that you have SSH keys set up for Git and that you've added them to your SSH agent.\n")
+		utils.Warn("You can use `plural crypto ssh-keygen` to create your first SSH keys then upload the public key to your Git provider.\n")
+		return true, fmt.Errorf("found non-ssh upstream")
 	}
 
 	return true, nil
