0a7c730eccc501f476cddb3b9606009bfb61780a
Lukasz Zajaczkowski <zreigz@gmail.com>
2026-07-15 11:51:58 +0200

    implement templated agentHelmValues

--- pkg/console/settings.go
+++ pkg/console/settings.go
@@ -2,6 +2,7 @@ package console
 
 import (
 	"fmt"
+	"strings"
 
 	gqlclient "github.com/pluralsh/console/go/client"
 	"github.com/pluralsh/plural-cli/pkg/api"
@@ -20,6 +21,14 @@ func (c *consoleClient) UpdateDeploymentSettings(attr gqlclient.DeploymentSettin
 }
 
 func (c *consoleClient) GetGlobalSettings() (*gqlclient.DeploymentSettingsFragment, error) {
+	settings, err := c.getGlobalSettings()
+	if err != nil && IsUnknownGraphQLField(err, "agentHelmValuesTemplateable") {
+		return c.GetGlobalSettingsMinimal()
+	}
+	return settings, err
+}
+
+func (c *consoleClient) getGlobalSettings() (*gqlclient.DeploymentSettingsFragment, error) {
 	resp, err := c.client.GetDeploymentSettings(c.ctx)
 	if err != nil {
 		return nil, api.GetErrorResponse(err, "GetDeploymentSettings")
@@ -28,4 +37,36 @@ func (c *consoleClient) GetGlobalSettings() (*gqlclient.DeploymentSettingsFragme
 		return nil, fmt.Errorf("returned GetDeploymentSettings object is nil")
 	}
 	return resp.DeploymentSettings, nil
+}
+
+func (c *consoleClient) GetGlobalSettingsMinimal() (*gqlclient.DeploymentSettingsFragment, error) {
+	resp, err := c.client.GetDeploymentSettingsMinimal(c.ctx)
+	if err != nil {
+		return nil, api.GetErrorResponse(err, "GetDeploymentSettingsMinimal")
+	}
+	if resp == nil {
+		return nil, fmt.Errorf("returned GetDeploymentSettingsMinimal object is nil")
+	}
+	return toDeploymentSettingsFragment(resp.DeploymentSettings), nil
+}
+
+func toDeploymentSettingsFragment(minimal *gqlclient.DeploymentSettingsMinimalFragment) *gqlclient.DeploymentSettingsFragment {
+	if minimal == nil {
+		return nil
+	}
+
+	return &gqlclient.DeploymentSettingsFragment{
+		AgentHelmValues: minimal.AgentHelmValues,
+		AgentVsn:        minimal.AgentVsn,
+	}
+}
+
+func IsUnknownGraphQLField(err error, field string) bool {
+	if err == nil {
+		return false
+	}
+
+	msg := err.Error()
+	quotedField := `"` + field + `"`
+	return strings.Contains(msg, "Cannot query field") && strings.Contains(msg, quotedField)
 }
